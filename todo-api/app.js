b0696caf3c97915e4187db29e6d91ab3acf90252
Anselme Donato <anselme.donato2@gmail.com>
2023-11-18 16:25:57 +0200

    Initial commit (working API without caching)

---
+++ todo-api/app.js
@@ -0,0 +1,47 @@
+import * as todoService from "./services/todoService.js"; 
+// import { cacheMethodCalls } from "./util/cacheUtil.js";
+
+// const cachedtodoService = cacheMethodCalls(todoService, ["addTodo"]); //Breaks everything for some reason...
+
+const urlMapping = [
+  {
+    method: "GET",
+    pattern: new URLPattern({ pathname: "/todos" }),
+    fn: todoService.getTodos,
+    // fn: cachedtodoService.getTodos,
+  },
+  {
+    method: "POST",
+    pattern: new URLPattern({ pathname: "/todos" }),
+    fn: todoService.addTodo,
+    // fn: cachedtodoService.addTodo,
+  },
+  {
+    method: "DELETE",
+    pattern: new URLPattern({ pathname: "/todos/:id" }),
+    fn: todoService.delTodo,
+    // fn: cachedtodoService.delTodo,
+  },
+];
+
+const handleRequest = async (request) => {
+	const mapping = urlMapping.find(
+	  (um) => um.method === request.method && um.pattern.test(request.url)
+	);
+  
+	if (!mapping) {
+	  return new Response("Not found", { status: 404 });
+	}
+  
+	const mappingResult = mapping.pattern.exec(request.url);
+	try {
+    return await mapping.fn(request, mappingResult);
+	} catch (e) {
+	  console.log(e);
+	  return new Response(e.stack, { status: 500 }); 
+	}
+};
+
+console.log("starting"); 
+const portConfig = { port: 7777, hostname: '0.0.0.0'}; 
+Deno.serve(portConfig, handleRequest);
---
+++ todo-api/services/todoService.js
@@ -0,0 +1,40 @@
+import {postgres} from "../deps.js";
+
+const sql = postgres({});
+
+const getTodos = async (_request, _mappingResult) => {
+	const todos = await sql`SELECT * FROM todos`;
+	return Response.json(todos); 
+};
+
+const addTodo = async (request, _mappingResult) => {
+  let item;
+  try {
+    const jsonData = await request.json();
+    item = jsonData.item.trim();
+  } catch (e) {
+	console.log(e); 
+    return new Response("Bad request", { status: 400 });
+  }
+
+  if (!item || item.length === 0) {
+    return new Response("Bad request", { status: 400 });
+  }
+
+  await sql`INSERT INTO todos (item) VALUES (${ item })`;
+  return new Response("OK", { status: 200 });
+};
+
+const delTodo = async (_request, mappingResult) => {
+  const id = mappingResult.pathname.groups.id;
+  const todos = await sql`SELECT * FROM todos WHERE id = ${ id }`;
+
+  if (todos.length == 0) {
+	  return new Response("Not found", { status: 404 });
+  }
+
+  await sql`DELETE FROM todos WHERE id = ${ id }`
+  return new Response("OK", { status: 200 });
+};
+
+export { getTodos, addTodo, delTodo }; 
