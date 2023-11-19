6cf120782eb4214569315e2d087eaafc5929ac69
Anselme Donato <anselme.donato2@gmail.com>
2023-11-19 12:39:50 +0200

    Adding UI for reading and adding items

---
+++ todo-ui/src/components/Items.svelte
@@ -0,0 +1,47 @@
+<script>
+	let item = "";
+	const getTodos = async () => {
+	  const response = await fetch("/api/todos");
+	  return await response.json();
+	};
+  
+	let todosPromise = getTodos();
+	
+	const addTodo = async () => {
+		if (item.length == 0) {
+			return; 
+		}
+		
+		const newTodo = { item }; 
+		
+		await fetch("/api/todos", {
+			method: "POST", 
+			body: JSON.stringify(newTodo), 
+		}); 
+		
+		item = "";
+
+		todosPromise = getTodos();
+	};
+  </script>
+  
+  <h1>Items</h1>
+  
+  <h1>Todos</h1>
+  
+  <input type="text" bind:value={item} />
+  <button on:click={addTodo}>Add todo</button>
+  
+  {#await todosPromise}
+	<p>Loading items</p>
+  {:then todos}
+	{#if todos.length == 0}
+	  <p>No items available</p>
+	{:else}
+	  <ul>
+		{#each todos as todo}
+		  <li>{todo.item}</li>
+		{/each}
+	  </ul>
+	{/if}
+  {/await}
